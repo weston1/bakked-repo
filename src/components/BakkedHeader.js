0db32ea2d807c86a2fc7a1ed2e6ff0c0f28eadd1
w <>
2020-08-24 12:22:57 -0400

    Slider update, gatsby --v

--- src/components/BakkedHeader.js
+++ src/components/BakkedHeader.js
@@ -2,21 +2,57 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
+import { CgAdidas } from 'react-icons/cg'
 
 import './BakkedHeader.css'
 
 export default props => {
-  const [sliderRef] = useKeenSlider({ loop: true })
+  const [pause, setPause] = React.useState(false)
+  const timer = React.useRef()
+  const [sliderRef, slider] = useKeenSlider({
+    loop: true,
+    duration: 1000,
+    dragStart: () => {
+      setPause(true)
+    },
+    dragEnd: () => {
+      setPause(false)
+    }
+  })
+
+  React.useEffect(() => {
+    sliderRef.current.addEventListener('mouseover', () => {
+      setPause(true)
+    })
+    sliderRef.current.addEventListener('mouseout', () => {
+      setPause(false)
+    })
+  }, [sliderRef])
+
+  React.useEffect(() => {
+    timer.current = setInterval(() => {
+      if (!pause && slider) {
+        slider.next()
+      }
+    }, 2000)
+    return () => {
+      clearInterval(timer.current)
+    }
+  }, [pause, slider])
 
   return (
     <div className="BakkedHeader">
-      <div
-        ref={sliderRef}
-        className="BakkedHeader--Slider relative keen-slider"
-      >
-        <div className="keen-slider__slide number-slide1">FPO</div>
-        <div className="keen-slider__slide number-slide2">FPO</div>
+      <div ref={sliderRef} className="keen-slider">
+        <div className="keen-slider__slide number-slide1">
+          <CgAdidas />
+        </div>
+        <div className="keen-slider__slide number-slide2">2</div>
+        <div className="keen-slider__slide number-slide3">3</div>
+        <div className="keen-slider__slide number-slide4">4</div>
+        <div className="keen-slider__slide number-slide5">5</div>
+        <div className="keen-slider__slide number-slide6">6</div>
       </div>
+
       <div className="row">
         <div className="col-lg-2">
           <img
